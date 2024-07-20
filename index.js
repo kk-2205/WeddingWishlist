8dddf80bb01ed00c6e4a82724119deed32f25abb
Karishma Kamalahasan <95235462+kk-2205@users.noreply.github.com>
2024-07-20 05:40:42 +0000

    change index.js

--- index.js
+++ index.js
@@ -53,10 +53,7 @@ app.get('/wishlist/add', (req, res) => {
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
   console.log('MongoDB connected');
   app.listen(PORT, () => {
