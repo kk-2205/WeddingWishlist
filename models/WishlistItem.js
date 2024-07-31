e5988b972cd5012d5c3d0a58d959c302e10f4654
Karishma Kamalahasan <95235462+kk-2205@users.noreply.github.com>
2024-07-31 06:34:21 +0000

    option feature

--- models/WishlistItem.js
+++ models/WishlistItem.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const WishlistItemSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  image: { type: String, required: true },
+  image: { type: String, required: false },
   amazonLink: { type: String, required: true },
-  price: { type: Number, required: true },
+  price: { type: Number, required: false },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Reference to the user
 });
 
